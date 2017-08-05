80965e3f18897b7ecd758d91d360b2c812325267
Roberto Barbosa <robertojf95@gmail.com>
2017-08-05 14:40:23 -0300

    adicionado campo de favorito na igreja

--- models/Usuario.js
+++ models/Usuario.js
@@ -77,6 +77,9 @@ const Usuario = db.define('usuario', {
     },
     'distancia': {
         type: Sequelize.VIRTUAL
+    },
+    'favorito': {
+        type : Sequelize.VIRTUAL
     }
 });
 module.exports = Usuario;
