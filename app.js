1c0498fe7cb5bea3471fa8b7021108aa6944e6d9
Roberto Barbosa <robertojf95@gmail.com>
2017-08-05 15:06:48 -0300

    Image diretorio

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(Cors());
 app.use(BodyParser.json({limit : '6mb'}));
 app.use(BodyParser.urlencoded({ extended: true, limit : '6mb' }));
 
-app.use(Express.static('ahrm_v2'));
+app.use('/imagem', Express.static(__dirname + '/uploads' ));
 
 //Rotas da api
 //Igreja || Usuario
