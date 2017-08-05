be11a65d0b8f30ffada25cab479b5260d07c0381
Roberto Barbosa <robertojf95@gmail.com>
2017-08-05 15:22:08 -0300

    add

--- routes/usuario.js
+++ routes/usuario.js
@@ -41,31 +41,39 @@ router.get('/:id', (req, res, next) => {
 
 });
 
+var map = { "â": "a", "Â": "A", "à": "a", "À": "A", "á": "a", "Á": "A", "ã": "a", "Ã": "A", "ê": "e", "Ê": "E", "è": "e", "È": "E", "é": "e", "É": "E", "î": "i", "Î": "I", "ì": "i", "Ì": "I", "í": "i", "Í": "I", "õ": "o", "Õ": "O", "ô": "o", "Ô": "O", "ò": "o", "Ò": "O", "ó": "o", "Ó": "O", "ü": "u", "Ü": "U", "û": "u", "Û": "U", "ú": "u", "Ú": "U", "ù": "u", "Ù": "U", "ç": "c", "Ç": "C" };
+
+function removerAcentos(s) { return s.replace(/[\W\[\] ]/g, function (a) { return map[a] || a }) };
+
 router.post('/', (req, res, next) => {
-    let foto = req.body.usuario.foto.replace(/^data:image\/\w+;base64,/, '');
-    let data = new Date();
-    let mes = data.getMonth() + 1;
-    data = data.getDate() + "_" + mes + "_" + data.getFullYear();
-    let nomedocumento = req.body.usuario.nome.split(' ').join( "");
-    let nomeArquivo = nomedocumento + data + '.jpg';
-    FileSystem.writeFile('uploads/usuarios/' + nomeArquivo, foto, {
-        encoding: 'base64'
-    }, function (error) {
-        if (error) {
-            res.send(error);
-        }
-        req.body.usuario.foto = nomeArquivo;
-        Usuario.create(req.body.usuario, {
-            include: [{
-                association: CreatorAssociation
-            }]
-        }).then((usuario) => {
-            delete req.body.usuario.senha;
-	console.log(usuario);            
-res.json(req.body.usuario);
-        }).catch((error) => res.send(error));
-    });
+
+    console.log(removerAcentos(ParóquiaSantaLuziadeSiracusa17_7_2017));
+//     let foto = req.body.usuario.foto.replace(/^data:image\/\w+;base64,/, '');
+//     let data = new Date();
+//     let mes = data.getMonth() + 1;
+//     data = data.getDate() + "_" + mes + "_" + data.getFullYear();
+//     let nomedocumento = req.body.usuario.nome.split(' ').join( "");
+//     let nomeArquivo = nomedocumento + data + '.jpg';
+//     FileSystem.writeFile('uploads/usuarios/' + nomeArquivo, foto, {
+//         encoding: 'base64'
+//     }, function (error) {
+//         if (error) {
+//             res.send(error);
+//         }
+//         req.body.usuario.foto = nomeArquivo;
+//         Usuario.create(req.body.usuario, {
+//             include: [{
+//                 association: CreatorAssociation
+//             }]
+//         }).then((usuario) => {
+//             delete req.body.usuario.senha;
+// 	console.log(usuario);            
+// res.json(req.body.usuario);
+//         }).catch((error) => res.send(error));
+//     });
 });
+
+
 
 router.put('/:id', (req, res, next) => {
     let foto = req.body.usuario.foto;
