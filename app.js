200c91e37311922906b060de6f3b638f54a47655
Roberto Barbosa <robertojf95@gmail.com>
2017-08-05 14:51:40 -0300

    express static

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(Cors());
 app.use(BodyParser.json({limit : '6mb'}));
 app.use(BodyParser.urlencoded({ extended: true, limit : '6mb' }));
 
-app.use(Express.static(Path.join(__dirname, 'public')));
+app.use(Express.static( 'public'));
 
 //Rotas da api
 //Igreja || Usuario
